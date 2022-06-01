b23af6d41c568d0e0c215ff5575bea3cb721a23d
Magyar-Hunor Tamas <hunor@makers.tech>
2022-06-01 01:29:13 +0100

    Fully refactored car route to delegate controller responsibilities into CarController

--- controllers/car.controller.js
+++ controllers/car.controller.js
@@ -1,8 +1,40 @@
 const Car = require('../models/Car');
 
 // Retrieve all Cars from the database
-exports.findAll = (req, res) => {
+exports.findAllCars = (req, res) => {
     Car.find()
       .then(cars => res.json(cars))
       .catch(err => res.status(404).json({ nocarssfound: 'No Cars found'}));
+}
+
+// Retrieve a single Car from the database
+exports.findOneCar = (req, res) => {
+    Car.findById(req.params.id)
+    .then(car => res.json(car))
+    .catch(err => res.status(404).json({ nocarfound: 'No Car found' }));
+}
+
+// Create a new Car
+exports.createCar = (req, res) => {
+    Car.create(req.body)
+    .then(car => res.json({ msg: 'Car added successfully' }))
+    .catch(err => res.status(400).json({ error: 'Unable to add this car' }));
+}
+
+// Update a Car
+exports.updateCar = (req, res) => {
+    Car.findByIdAndUpdate(req.params.id, req.body)
+    .then(car => res.json({ 
+        msg: 'Updated successfully'
+    }))
+    .catch(err =>
+      res.status(400).json({ error: 'Unable to update the Database' })
+    );
+}
+
+// Delete a Car
+exports.deleteCar = (req, res) => {
+    Car.findByIdAndRemove(req.params.id, req.body)
+    .then(car => res.json({ mgs: 'Car deleted successfully' }))
+    .catch(err => res.status(404).json({ error: 'No such Car' }));
 }
--- routes/api/cars.js
+++ routes/api/cars.js
@@ -3,9 +3,6 @@
 const express = require('express');
 const router = express.Router();
 
-// Load Car model
-const Car = require('../../models/Car');
-
 const CarController = require('../../controllers/car.controller');
 
 // @route GET api/cars/test
@@ -16,46 +13,26 @@ router.get('/test', (req, res) => res.send('car route testing!'));
 // @route GET api/cars
 // @description Get all cars
 // @access Public
-router.get('/', CarController.findAll);
+router.get('/', CarController.findAllCars);
 
 // @route GET api/cars/:id
 // @description Get single car by id
 // @access Public
-router.get('/:id', (req, res) => {
-  Car.findById(req.params.id)
-    .then(car => res.json(car))
-    .catch(err => res.status(404).json({ nocarfound: 'No Car found' }));
-});
+router.get('/:id', CarController.findOneCar);
 
 // @route GET api/cars
 // @description add/save car
 // @access Public
-router.post('/', (req, res) => {
-  Car.create(req.body)
-    .then(car => res.json({ msg: 'Car added successfully' }))
-    .catch(err => res.status(400).json({ error: 'Unable to add this car' }));
-});
+router.post('/', CarController.createCar);
 
-// @route GET api/cars/:id
+// @route PUT api/cars/:id
 // @description Update car
 // @access Public
-router.put('/:id', (req, res) => {
-  Car.findByIdAndUpdate(req.params.id, req.body)
-    .then(car => res.json({ 
-        msg: 'Updated successfully'
-    }))
-    .catch(err =>
-      res.status(400).json({ error: 'Unable to update the Database' })
-    );
-});
+router.put('/:id', CarController.updateCar);
 
-// @route GET api/cars/:id
+// @route DELETE api/cars/:id
 // @description Delete car by id
 // @access Public
-router.delete('/:id', (req, res) => {
-  Car.findByIdAndRemove(req.params.id, req.body)
-    .then(car => res.json({ mgs: 'Book entry deleted successfully' }))
-    .catch(err => res.status(404).json({ error: 'No such a car' }));
-});
+router.delete('/:id', CarController.deleteCar);
 
 module.exports = router;
